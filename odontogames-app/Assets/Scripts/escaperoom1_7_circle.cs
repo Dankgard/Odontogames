a2a00aa0af04356f20cd898a64e54f908ef7f141
Dankgard <dangar18@ucm.es>
2023-03-01 18:22:13 +0100

    Continuando minijuego 7

--- odontogames-app/Assets/Scripts/escaperoom1_7_circle.cs
+++ odontogames-app/Assets/Scripts/escaperoom1_7_circle.cs
@@ -5,26 +5,41 @@ using UnityEngine;
 public class escaperoom1_7_circle : MonoBehaviour
 {
     public GameObject letter;
-    int letters = 27;
+    int numberOfLetters = 26;
     public float radius = 5f;
+    string circleLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    GameObject[] letters;
+
+    int activeLetter = 0;
+
+    public Material[] colors;
 
     void Awake()
     {
+        letters = new GameObject[27];
         SpawnCircle();
     }
 
     void SpawnCircle()
     {
-        float angleIncrement = 360f / letters;
+        float angleIncrement = 360f / numberOfLetters;
 
-        for(int i=0;i<letters;i++)
+        for(int i=0;i<numberOfLetters;i++)
         {
-            float angle = i * angleIncrement * Mathf.Deg2Rad;
+            float angle = (270f + i * angleIncrement) * Mathf.Deg2Rad;
             Vector3 position = new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * radius;
-            GameObject spawnedLetter = Instantiate(letter, transform.position + position, Quaternion.identity);
-            spawnedLetter.transform.SetParent(transform);
+            letters[i] = Instantiate(letter, transform.position + position, Quaternion.identity);
+            letters[i].transform.SetParent(transform);
+            letters[i].transform.GetChild(1).GetComponent<TextMesh>().text = circleLetters[i].ToString();
         }
 
         transform.Rotate(Vector3.right, 90f);
+        LoadLetterMaterial(1);
+    }
+
+    void LoadLetterMaterial(int color)
+    {
+        letters[activeLetter].transform.GetChild(0).GetComponent<Renderer>().material = colors[color];
     }
 }
