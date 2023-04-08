481d67b5bd30f1de4fa688d43d8f804eb44d6310
AlejandroPla <aplazer@ucm.es>
2023-04-08 09:58:51 +0200

    listas de grupos y usuarios terminadas

--- odontogames-app/Assets/Scripts/GlobalUserList.cs
+++ odontogames-app/Assets/Scripts/GlobalUserList.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using StrapiForUnity;
 using UnityEngine.UI;
 
@@ -9,6 +10,8 @@ public class GlobalUserList : MonoBehaviour
 {
     public GameObject userPrefab;
     public GameObject viewport;
+
+    public GameObject userPanel;
 
     private StrapiUser[] users = null;
     private GameObject[] userID = null;
@@ -49,6 +52,7 @@ public class GlobalUserList : MonoBehaviour
 
     public void BuildUserScreen()
     {
+        userPanel.SetActive(false);
         users = StrapiComponent._instance.GetUsers();
         Debug.Log(users.Length);
         userID = new GameObject[users.Length];
@@ -58,17 +62,31 @@ public class GlobalUserList : MonoBehaviour
             GameObject user = Instantiate(userPrefab, new Vector3(0, viewport.transform.position.y - 50 * i, 0), Quaternion.identity, viewport.transform);
             user.transform.GetChild(0).GetComponent<Text>().text = users[i].username;
             user.transform.GetChild(1).GetComponent<Text>().text = users[i].firstname;
-
-            //Vector3 newPos = user.transform.position;
-            //newPos.x = 0;
-            //user.transform.position = newPos;
-            //Debug.Log("User " + i + ": " + user.transform.position);
+            user.GetComponent<Button>().onClick.AddListener(() => OnUserButtonkPressed());
 
             userID[i] = user;
         }
     }
 
+    public void OnUserButtonkPressed()
+    {
+        Button button = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+        int id = System.Array.IndexOf(userID, button.gameObject);
+
+        userPanel.transform.GetChild(0).GetComponent<Text>().text = "Username: " + users[id].username;
+        userPanel.transform.GetChild(1).GetComponent<Text>().text = "Name: " + users[id].firstname + " " + users[id].lastname;
+        userPanel.transform.GetChild(2).GetComponent<Text>().text = "Email: " + users[id].email;
+        userPanel.transform.GetChild(3).GetComponent<Text>().text = "Team: " + users[id].group;
+        userPanel.transform.GetChild(4).GetComponent<Text>().text = "Joined: " + users[id].CreatedAt();
+        userPanel.SetActive(true);
+    }
+
     public void OnBackPressed()
+    {
+        userPanel.SetActive(false);
+    }
+
+    public void OnClosePressed()
     {
         MySceneManager.instance.LoadScene("MainMenu");
     }
--- odontogames-app/Assets/Scripts/TeamsList.cs
+++ odontogames-app/Assets/Scripts/TeamsList.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using StrapiForUnity;
 
 public class TeamsList : MonoBehaviour
 {
     public GameObject groupPrefab;
     public GameObject viewport;
+
+    public GameObject teamsPanel;
+    public GameObject newTeamPanel;
 
     private StrapiTeamsData[] teams = null;
     private GameObject[] teamID = null;
@@ -35,8 +39,29 @@ public class TeamsList : MonoBehaviour
             teamData.transform.GetChild(0).GetComponent<Text>().text = team.teamname;
             teamData.transform.GetChild(1).GetComponent<Text>().text = team.numplayers.ToString();
 
+            teamData.GetComponent<Button>().onClick.AddListener(() => OnTeamButtonkPressed());
+
             teamID[i] = teamData;
         }
+    }
+
+    public void OnTeamButtonkPressed()
+    {
+        Button button = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+        int id = System.Array.IndexOf(teamID, button.gameObject);
+
+        StrapiUserTeam team = teams[id].attributes;
+        teamsPanel.transform.GetChild(0).GetComponent<Text>().text = "Team name: " + team.teamname;
+        teamsPanel.transform.GetChild(1).GetComponent<Text>().text = "Creator: " + team.creator;
+        teamsPanel.transform.GetChild(2).GetComponent<Text>().text = "Score: " + team.teamscore.ToString();
+        teamsPanel.transform.GetChild(3).GetComponent<Text>().text = "Players: " + team.numplayers.ToString();
+        teamsPanel.transform.GetChild(4).GetComponent<Text>().text = "Joined: " + team.CreatedAt();
+        teamsPanel.SetActive(true);
+    }
+
+    public void OnClosePressed()
+    {
+        teamsPanel.SetActive(false);
     }
 
     private IEnumerator GetTeamsCoroutine()
@@ -48,5 +73,10 @@ public class TeamsList : MonoBehaviour
     public void OnBackPressed()
     {
         MySceneManager.instance.LoadScene("MainMenu");
+    }
+
+    public void OnNewTeamPressed()
+    {
+        newTeamPanel.SetActive(false);
     }
 }
